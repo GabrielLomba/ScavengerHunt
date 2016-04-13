58f09ef45f4f4a5651faecc16ad608df4da6f0be
rbh9dm <rbh9dm@virginia.edu>
2016-04-13 18:04:58 -0400

    gps

--- ScavengerHunt/app/src/main/java/rbh9dm/cs4720/com/scavengerhunt/More_info.java
+++ ScavengerHunt/app/src/main/java/rbh9dm/cs4720/com/scavengerhunt/More_info.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +13,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,8 +21,13 @@ import android.widget.TextView;
 
 public class More_info extends AppCompatActivity {
 
-    private double lat = 50;
-    private double lon = 50;
+    private double lat = 38.03;
+    private double lon = -78.52;
+    private double currLat = 0;
+    private double currLon = 0;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +45,8 @@ public class More_info extends AppCompatActivity {
         TextView description = (TextView) findViewById(R.id.Mdescription);
         description.setText(HuntItems.itemList.get(pos).getDescription());
 
-        TextView location = (TextView) findViewById(R.id.Mlocation);
-        location.setText(HuntItems.itemList.get(pos).getNameOfLocation());
+        TextView loc = (TextView) findViewById(R.id.Mlocation);
+        loc.setText(HuntItems.itemList.get(pos).getNameOfLocation());
 
         CheckBox chk = (CheckBox) findViewById(R.id.chkcamera);
 
@@ -52,26 +59,40 @@ public class More_info extends AppCompatActivity {
             chk.setText("Required");
         chk.setChecked(HuntItems.itemList.get(pos).isLocationOk());
 
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                currLat = location.getLatitude();
+                currLon = location.getLongitude();
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        if (ActivityCompat.checkSelfPermission(More_info.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(More_info.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            TextView distText = (TextView) findViewById(R.id.distance);
+            distText.setText("GPS not enabled");
+        }
+        else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+
         Button locButton = (Button) this.findViewById(R.id.location);
         locButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LocationManager locationManager = (LocationManager) More_info.this.getSystemService(Context.LOCATION_SERVICE);
-                if (ActivityCompat.checkSelfPermission(More_info.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(More_info.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    TextView distText = (TextView) findViewById(R.id.distance);
-                    distText.setText("GPS not enabled");
-                }
-                else {
-                    Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    TextView distText = (TextView) findViewById(R.id.distance);
-                    float[] dist = new float[100];
-                    Location.distanceBetween(location.getLatitude(), location.getLongitude(), lat, lon, dist);
-                    distText.setText(""+dist[0]);
-                }
+                float[] dist = new float[3];
+                Location.distanceBetween(lat, lon, currLat, currLon, dist);
+                TextView distText = (TextView) findViewById(R.id.distance);
+                distText.setText(""+currLat+", "+currLon+": "+dist[0]);
             }
         });
     }
-
-
 
 }
